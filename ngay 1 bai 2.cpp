9e479c9b2f154851d89c658607e42a98c4ce0ee4
hieult123 <81707523+hieult123@users.noreply.github.com>
2021-04-07 01:19:14 +0700

    Add files via upload

---
+++ ngay 1 bai 2.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main ()
+{
+
+int n;
+printf (" nhap vap chieu cao cua ban ");
+scanf ( "%d", &n);
+printf (" vay thi chieu cao cua ban la %d.%d m", n/100, n%100);
+return 0;
+}
